93a98f0df6efa28ef08bfd166f311841f6510ba4
aferreiradominguez <aferreiradominguez@eniac27.danielcastelao.org>
2014-10-15 11:37:44 +0200

    1ªparte ended+mail2ndhalf

--- src/boletin4/Boletin4.java
+++ src/boletin4/Boletin4.java
@@ -13,12 +13,22 @@ public class Boletin4 {
         System.out.println("distancia percorrida="+consumo2.km+"\n"+"combustible consumido="+consumo2.litros+"\n"+"velocidade media="+consumo2.vmed+"\n"+"prezo gasolina="+consumo2.pGas);
         //probo os sets
         consumo2.setKm(10);
-        consumo2.setLitros(20);
+        consumo2.setLitros(50);
         consumo2.setVmed(50);
-        consumo2.setPGas(1.50);
+        consumo2.setPGas(1.57);
         System.out.println("distancia percorrida="+consumo2.km+"\n"+"combustible consumido="+consumo2.litros+"\n"+"velocidade media="+consumo2.vmed+"\n"+"prezo gasolina="+consumo2.pGas);
         System.out.println("gasto en €="+consumo2.consumoEuros(1.5,10));
-        System.out.println("consumoMedio="+consumo2.consumoMedio(5,10));
+        System.out.println("consumoMedio="+consumo2.consumoMedio(50, 100));
+        System.out.println("Velocidade media="+consumo2.vmed);
+        
+/*a)    Crea un obxecto, de tipo consume, utilizando o constructor sen parámetros
+b)    Dalle a litros o valor 50 e a prezo da gasolina 1.57
+c)    Crea un obxecto, tipo consumo, utilizando o contructor con todos os parámetros
+d)    Visualiza, a  través do 2º obxecto, o consumo medio
+e)    Varia o valor dos litros consumidos do 2º obxecto.
+f)     Visualiza a velocidade media do 2º obxecto.*/
+        //
+        
         
         
         
